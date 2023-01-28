f58c6b6f673b19bfc0b2ca2da669200cec4f3073
Narayan <laxminarayanreddy432@gmail.com>
2023-01-28 20:45:02 +0530

    added-firebase-hosting-n-github-actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">{"Hello world! portfolio site v2"}</h1>
+    <div className="flex h-screen items-center justify-center ">
+      <h1 className="text-3xl font-bold">
+        {"Hello world! new portfolio site"}
+      </h1>
     </div>
   );
 }
