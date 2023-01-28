79e568b58efc86776ab6fc3111f348aa262921f2
Narayan <laxminarayanreddy432@gmail.com>
2023-01-29 00:47:17 +0530

    test: github action workflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center ">
+    <div className="flex flex-col h-screen items-center justify-center ">
       <h1 className="text-3xl font-bold">
-        {"Hello world! new portfolio site"}
+        🛠️ under development | check out v1 👇
+      </h1>
+      <h1
+        onClick={() => {
+          window.open("https://narayann.me");
+        }}
+        className="text-3xl font-bold p-3 cursor-pointer hover:text-teal-600"
+      >
+        narayann.me
       </h1>
     </div>
   );
